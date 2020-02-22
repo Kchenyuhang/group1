aa36ac2c32736bef9d40284236c8f7272a32876b
yhChen <kchenyuhang@gmail.com>
2020-02-22 23:54:25 +0800

    bug修复

---
+++ src/com/soft1851/group1/functional/dyf/test.java
@@ -0,0 +1,9 @@
+package com.soft1851.group1.functional.dyf;
+
+/**
+ * @Author yhChen
+ * @Description
+ * @Date 2020/2/22
+ */
+public class test {
+}
---
+++ src/com/soft1851/group1/functional/wzw/test.java
@@ -0,0 +1,9 @@
+package com.soft1851.group1.functional.wzw;
+
+/**
+ * @Author yhChen
+ * @Description
+ * @Date 2020/2/22
+ */
+public class test {
+}
